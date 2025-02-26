98728c7285125debd93650fe64ab57f23c9865e3
Maqsood Alam <malam84@gmail.com>
2025-02-26 14:19:10 +0800

    Update LoginExampleApplicationTests.java

--- src/test/java/com/jts/login/LoginExampleApplicationTests.java
+++ src/test/java/com/jts/login/LoginExampleApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class LoginExampleApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//@Test
+	//void contextLoads() {
+	//}
 
 }
